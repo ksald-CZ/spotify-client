c9b41e2aac209713e5d6fa453f7ed9286a8150f6
ksald-CZ <dlaskdan@gmail.com>
2023-06-09 21:33:55 +0200

    mozna neco

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,37 @@
-use rspotify::client::Spotify;
-use rspotify::oauth2::{CredentialsBuilder, OAuthBuilder};
+// Import necessary libraries
+use gtk::{self, prelude::*, Button, Window, WindowType};
+use rspotify::{self, prelude::*, OAuthBuilder, Credentials};
 
-#[tokio::main]
-async fn main() {
-    let creds = CredentialsBuilder::default()
+// Main function
+fn main() {
+    // Initialize GTK and create a window
+    gtk::init().expect("Failed to initialize GTK.");
+    let window = Window::new(WindowType::Toplevel);
+    window.set_title("Spotify Client");
+    window.set_default_size(800, 600);
+
+    // Create UI components (buttons, labels, etc.) using GTK
+
+    // Connect to Spotify API
+    let creds = Credentials::default()
         .client_id("2f0d27d3267c44fca254e7e4f74ff44b")
         .client_secret("6fdb28d5508a4c5da3ca064a6a7ae2ae")
-        .build()
-        .unwrap();
-
+        .build();
     let oauth = OAuthBuilder::default()
-        .scope("user-read-private")
-        .build()
-        .unwrap();
-
-    let mut spotify = Spotify::default()
         .credentials(creds)
-        .oauth(oauth)
-        .build()
-        .unwrap();
+        .scope("user-read-private playlist-read-private")
+        .build();
 
-    spotify.prompt_for_user_token().await.unwrap();
+    // Handle button click events
+    button.connect_clicked(|_| {
+        // Implement logic for button click, such as searching for songs or controlling playback
+    });
 
-    let me = spotify.me().await.unwrap();
-    println!("Hello, {}!", me.display_name.unwrap());
+    // Build the UI hierarchy and add components to the window
+
+    // Show the window
+    window.show_all();
+
+    // Start the GTK main event loop
+    gtk::main();
 }
