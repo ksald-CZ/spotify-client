7376243110d87cffd181b8fd864de6baf15645da
ksald-CZ <dlaskdan@gmail.com>
2023-06-16 21:12:31 +0200

    neco to dela

--- src/main.rs
+++ src/main.rs
@@ -1,37 +1,57 @@
-// Import necessary libraries
-use gtk::{self, prelude::*, Button, Window, WindowType};
-use rspotify::{self, prelude::*, OAuthBuilder, Credentials};
 
-// Main function
-fn main() {
-    // Initialize GTK and create a window
-    gtk::init().expect("Failed to initialize GTK.");
-    let window = Window::new(WindowType::Toplevel);
-    window.set_title("Spotify Client");
-    window.set_default_size(800, 600);
+use serde::{Deserialize, Serialize};
+use reqwest::header::{HeaderMap, HeaderValue};
+use reqwest::{Client, Response, Url};
 
-    // Create UI components (buttons, labels, etc.) using GTK
+#[derive(Deserialize, Serialize)]
+struct AccessToken {
+    access_token: String,
+    // Additional fields in the access token response
+}
 
-    // Connect to Spotify API
-    let creds = Credentials::default()
-        .client_id("2f0d27d3267c44fca254e7e4f74ff44b")
-        .client_secret("6fdb28d5508a4c5da3ca064a6a7ae2ae")
-        .build();
-    let oauth = OAuthBuilder::default()
-        .credentials(creds)
-        .scope("user-read-private playlist-read-private")
-        .build();
+#[tokio::main]
+async fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let client_id = "2f0d27d3267c44fca254e7e4f74ff44b";
+    let client_secret = "6fdb28d5508a4c5da3ca064a6a7ae2ae";
 
-    // Handle button click events
-    button.connect_clicked(|_| {
-        // Implement logic for button click, such as searching for songs or controlling playback
-    });
+    let client = Client::new();
+    let params = [
+        ("grant_type", "client_credentials"),
+        // Additional parameters
+    ];
 
-    // Build the UI hierarchy and add components to the window
+    let res = client
+        .post("https://accounts.spotify.com/api/token")
+        .basic_auth(client_id, Some(client_secret))
+        .form(&params)
+        .send()
+        .await?
+        .json::<serde_json::Value>()
+        .await?;
 
-    // Show the window
-    window.show_all();
+    let access_token = res["access_token"].as_str().unwrap();
 
-    // Start the GTK main event loop
-    gtk::main();
+    let url = Url::parse("https://api.spotify.com/v1/me/player/play")?;
+    let mut headers = HeaderMap::new();
+    headers.insert(
+        reqwest::header::AUTHORIZATION,
+        HeaderValue::from_str(&format!("Bearer {}", access_token))?,
+    );
+
+    let params = [
+        ("uris", "spotify:track:6rqhFgbbKwnb9MLmUQDhG6"),
+        // Additional parameters
+    ];
+
+    let res: Response = client
+        .put(url)
+        .headers(headers)
+        .json(&params)
+        .send()
+        .await?;
+
+    let response_text = res.text().await?;
+    println!("Response: {:?}", response_text);
+
+    Ok(())
 }
